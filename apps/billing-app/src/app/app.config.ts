ff78e1acd570bf24023826bc0e1e710f3facb6a3
Axel Mariano Bordon Alvarez <axel.mbordon@gmail.com>
2026-05-08 12:33:13 -0300

    Added tailwind and primeng

--- apps/billing-app/src/app/app.config.ts
+++ apps/billing-app/src/app/app.config.ts
@@ -3,8 +3,20 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
--- apps/billing-app/src/app/app.ts
+++ apps/billing-app/src/app/app.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, ButtonModule, DialogModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'billing-app';
+  protected isDialogVisible = false;
+
+  protected openDialog(): void {
+    this.isDialogVisible = true;
+  }
+
+  protected closeDialog(): void {
+    this.isDialogVisible = false;
+  }
 }
